04f6206513b508a589351f6a7a634fd0ea2e6313
YuriiLevchuk <yuriilevchuk04@gmail.com>
2025-08-26 19:43:28 +0200

    Added Info Bar functionality

--- resume/src/components/Pages/About.tsx
+++ resume/src/components/Pages/About.tsx
@@ -10,7 +10,48 @@ const About: React.FC = () => {
         and user-friendly applications. With a solid foundation in both front-end and back-end technologies, I can drive projects
          from initial concept through deployment. Beyond technical skills, I value collaboration, clear communication, and 
          adaptability - qualities that help me thrive in team-oriented and fast-paced environments.
-      </p> 
+      </p>
+      <div className="seperator"></div>
+
+      <h2>My Stack</h2>
+      <div className='columnWrapper'>
+        <div>
+          <h3>
+            <span>
+              <a>Front-End</a>
+              <img src=".\src\resources\icons\html.svg" />
+            </span>
+          </h3>
+          <ul>
+            <li>React.js</li>
+            <li>Next.js</li>
+            <li>TypeScript</li>
+            <li>Tailwind</li>
+          </ul>
+        </div>
+        <div>
+          <h3><span>
+            <a>Back-End</a>
+            <img src=".\src\resources\icons\server.svg" />
+          </span></h3>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Typescript</li>
+          </ul>
+        </div>
+        <div>
+          <h3><span>
+            Data Bases
+            <img src=".\src\resources\icons\database.svg" />
+          </span></h3>
+          <ul>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MySQL</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
