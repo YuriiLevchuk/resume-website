07a80fa55dde79d1dda380901f9f3e9dce3ead92
YuriiLevchuk <yuriilevchuk04@gmail.com>
2025-08-29 11:53:03 +0200

    Created contact page layout

---
+++ resume/src/components/ContactForm.tsx
@@ -0,0 +1,15 @@
+// import React, { useState } from "react";
+
+const ContactForm = () => {
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   email: "",
+  //   message: ""
+  // });
+
+  return(
+    <div></div>
+  )
+}
+
+export default ContactForm;
--- resume/src/components/Pages/Contacts.tsx
+++ resume/src/components/Pages/Contacts.tsx
@@ -1,8 +1,38 @@
 import type React from "react";
+import ContactForm from "../ContactForm";
 
 const Contacts: React.FC = () => {
   return <div>
-    <h1>My Contacts</h1>
+    <h1>Lets get in touch!</h1>
+    <h2>I speak <span>English, Ukrainian, Polish and Russian.</span> Feel free to contact me!</h2>
+    
+    <div className="columnWrapper">
+      <div>
+        <h3><span>
+          Location
+          <img src="icons\pin.svg" />
+        </span></h3>
+        <p>Czestochowa, Poland</p>
+      </div>
+      <div>
+        <h3><span>
+          Email
+          <img src="icons\contacts.svg" />
+        </span></h3>
+        <p>yurii.levchuk04@gmail.com</p>
+      </div>
+      <div>
+        <h3><span>
+          Phone
+          <img src="icons\phone.svg" />
+        </span></h3>
+        <p>+48 790 560 596</p>
+      </div>
+    </div>
+
+    <div className="seperator"></div>
+    <h2>Write me a message</h2>
+    <ContactForm/>
   </div>;
 };
 
